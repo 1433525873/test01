b3a41b5aa8d0a2a59ee7a02adfe0ef0406b05597
zs <zs@bjpowernode.com>
2022-03-27 15:52:46 +0800

    第三次提交

--- src/main/java/com/bjpowernode/domain/Student.java
+++ src/main/java/com/bjpowernode/domain/Student.java
@@ -4,4 +4,15 @@ public class Student {
     private String name;
     private int age;
     private String address;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+
 }
